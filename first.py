c9dd1e90c321302aa451896ef668f694b7d12f18
skrgod <82214696+skrgod@users.noreply.github.com>
2021-04-26 16:12:14 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,87 @@
+def fu1():
+	a = [0] * 20
+	a [0] = a [-1] = 1	
+	print (a)
+	
+	
+def fu2():
+	mass = []
+	for i in range(10):
+		mass.append(i%2)
+	print(mass)
+	
+	
+	
+def fu3():
+	mass = []
+	for i in range(1, 100, 2):
+		mass.append(i)
+	print(mass)
+	
+def fu4():
+	mass = []
+	x = int(input())
+	d = int(input())
+	for i in range(x, 100, d):
+		mass.append(i)
+	print(mass)
+
+def fu5():
+	mass = []
+	for i in range(0, 100, 2):
+		mass.append(i)
+	print(mass)
+
+def fu6():
+	mass = []
+	for i in range(102, 0, -3):
+		mass.append(i)
+	print(mass)
+
+def fu9():
+	mass = []
+	for i in range(1, 5):
+		mass.append(i**2)
+	print(mass)
+
+def fu8():
+	mass = []
+	a = int(input())
+	for i in range(2, a):
+		mass.append(i)
+		while i * i <= 
+	
+	
+
+	
+			
+			
+
+
+		
+	
+	
+		
+		
+while True:
+	xer = input ('Номер - ')
+	if xer == '1':
+		fu1()
+	elif xer == '2':
+		fu2()
+	elif xer == '3':
+		fu3()
+	elif xer == '4':
+		fu4()
+	elif xer == '5':
+		fu5()
+	elif xer == '6':
+		fu6()
+	elif xer == '9':
+		fu9()
+	elif xer == '8':
+		fu8()
+ 
+	
+	else:
+		print('Нет такой задачи')
